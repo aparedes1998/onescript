cf086a412d2071bdcb7f05eaa94d3ac1f4625a28
Francisco <franpiria42@gmail.com>
2021-11-24 17:14:33 -0300

    tests con Map y Set

--- src/parser/processors.js
+++ src/parser/processors.js
@@ -1,10 +1,10 @@
 const parseFunction = (parts) => {
-  const async = parts[0] === 'async';
+  const async = parts[0] === "async";
   if (async) {
     parts.shift();
   }
   parts.shift(); // 'function'
-  const generator = parts[0] === '*';
+  const generator = parts[0] === "*";
   if (generator) {
     parts.shift();
   }
@@ -18,137 +18,148 @@ const parseFunction = (parts) => {
  * node really represents a toke rather than an AST node.
  *
  * Please keep the functions sorted by name.
-*/
+ */
 const processors = {
   ArgList(_op, ...args) {
-    return args.filter((arg) => arg !== ',' && arg !== ')');
+    return args.filter((arg) => arg !== "," && arg !== ")");
   },
 
   ArrayExpression(...parts) {
     const elements = [];
     for (let i = 0; i < parts.length; i += 1) {
       const part = parts[i];
-      if (part !== ']') {
-        if (part === ',' || part === '[') {
-          if (parts[i + 1] === ',') {
+      if (part !== "]") {
+        if (part === "," || part === "[") {
+          if (parts[i + 1] === ",") {
             elements.push(null); // Empty element.
           }
-        } else if (part === '...') {
+        } else if (part === "...") {
           i += 1;
-          elements.push({ type: 'SpreadElement', argument: parts[i] });
+          elements.push({ type: "SpreadElement", argument: parts[i] });
         } else {
           elements.push(part);
         }
       }
     }
-    return { type: 'ArrayExpression', elements };
+    return { type: "ArrayExpression", elements };
   },
 
   ArrowFunction(...parts) {
-    const async = parts[0] === 'async';
+    const async = parts[0] === "async";
     if (async) {
       parts.shift();
     }
     const [params, _arrow, body] = parts;
     const id = null;
-    const expression = body.type !== 'BlockStatement';
+    const expression = body.type !== "BlockStatement";
     const generator = false;
     return {
-      type: 'ArrowFunctionExpression', async, params, body, id, expression, generator,
+      type: "ArrowFunctionExpression",
+      async,
+      params,
+      body,
+      id,
+      expression,
+      generator,
     };
   },
 
   AssignmentExpression(left, operator, right) {
-    return { type: 'AssignmentExpression', left, operator, right };
+    return { type: "AssignmentExpression", left, operator, right };
   },
 
   BinaryExpression(left, operator, right) {
-    if (left?.name === 'yield' && operator === '*') {
-      return { type: 'YieldExpression', argument: right, delegate: true };
+    if (left?.name === "yield" && operator === "*") {
+      return { type: "YieldExpression", argument: right, delegate: true };
     }
-    const type = /^(&&|\|\||\?\?)$/.test(operator) ? 'LogicalExpression'
-      : 'BinaryExpression';
+    const type = /^(&&|\|\||\?\?)$/.test(operator)
+      ? "LogicalExpression"
+      : "BinaryExpression";
     return { type, operator, left, right };
   },
 
   Block(_ob, ...parts) {
-    const body = parts.filter((part) => part !== '}');
-    return { type: 'BlockStatement', body };
+    const body = parts.filter((part) => part !== "}");
+    return { type: "BlockStatement", body };
   },
 
   BooleanLiteral(raw) {
-    return { type: 'Literal', value: raw === 'true', raw };
+    return { type: "Literal", value: raw === "true", raw };
   },
 
   BreakStatement(_break, _label) {
-    const label = _label === ';' ? null : _label;
-    return { type: 'BreakStatement', label };
+    const label = _label === ";" ? null : _label;
+    return { type: "BreakStatement", label };
   },
 
   CallExpression(callee, args) {
-    return { type: 'CallExpression', callee, arguments: args };
+    return { type: "CallExpression", callee, arguments: args };
   },
 
   ClassBody(_ob, body, _cb) {
-    return { type: 'ClassBody', body: _cb ? body : [] };
+    return { type: "ClassBody", body: _cb ? body : [] };
   },
 
   ClassDeclaration(_class, id, ...parts) {
     const body = parts.pop();
-    const superClass = parts[0] === 'extends' ? parts[1] : null;
-    return { type: 'ClassDeclaration', body, id, superClass };
+    const superClass = parts[0] === "extends" ? parts[1] : null;
+    return { type: "ClassDeclaration", body, id, superClass };
   },
 
   ClassExpression(_class, ...parts) {
     const body = parts.pop();
-    const superClass = parts[0] === 'extends' ? parts[1]
-      : (parts[1] === 'extends' ? parts[2] : null);
-    const id = parts.length === 1 || parts[1] === 'extends' ? parts[0] : null;
-    return { type: 'ClassExpression', body, id, superClass };
+    const superClass =
+      parts[0] === "extends"
+        ? parts[1]
+        : parts[1] === "extends"
+        ? parts[2]
+        : null;
+    const id = parts.length === 1 || parts[1] === "extends" ? parts[0] : null;
+    return { type: "ClassExpression", body, id, superClass };
   },
 
   ConditionalExpression(test, _qm, consequent, _colon, alternate) {
-    return { type: 'ConditionalExpression', test, consequent, alternate };
+    return { type: "ConditionalExpression", test, consequent, alternate };
   },
 
   ContinueStatement(_continue, _label) {
-    const label = _label === ';' ? null : _label;
-    return { type: 'ContinueStatement', label };
+    const label = _label === ";" ? null : _label;
+    return { type: "ContinueStatement", label };
   },
 
   DebuggerStatement() {
-    return { type: 'DebuggerStatement' };
+    return { type: "DebuggerStatement" };
   },
 
   DoStatement(_do, body, _while, test) {
-    return { type: 'DoWhileStatement', body, test };
+    return { type: "DoWhileStatement", body, test };
   },
 
   ExpressionStatement(expression) {
-    return { type: 'ExpressionStatement', expression };
+    return { type: "ExpressionStatement", expression };
   },
 
   ForInSpec(_op, left, _in, right) {
-    return { type: 'ForInStatement', left, right };
+    return { type: "ForInStatement", left, right };
   },
 
   ForOfSpec(_op, left, _of, right) {
-    return { type: 'ForOfStatement', left, right };
+    return { type: "ForOfStatement", left, right };
   },
 
   ForSpec(_op, init, ...parts) {
     return {
-      type: 'ForStatement',
-      init: init === ';' ? null : init,
-      test: parts[0] === ';' ? null : parts.shift(),
-      update: parts[1] === ')' ? null : parts[1],
+      type: "ForStatement",
+      init: init === ";" ? null : init,
+      test: parts[0] === ";" ? null : parts.shift(),
+      update: parts[1] === ")" ? null : parts[1],
     };
   },
 
   ForStatement(_for, ...parts) {
-    const async = parts[0] === 'await' ? !!(parts.shift()) : false;
+    const async = parts[0] === "await" ? !!parts.shift() : false;
     const [header, body] = parts;
-    if (header.type === 'ForOfStatement') {
+    if (header.type === "ForOfStatement") {
       header.await = async;
     }
     header.body = body;
@@ -156,15 +167,15 @@ const processors = {
   },
 
   FunctionDeclaration(...parts) {
-    return { ...parseFunction(parts), type: 'FunctionDeclaration' };
+    return { ...parseFunction(parts), type: "FunctionDeclaration" };
   },
 
   FunctionExpression(...parts) {
-    return { ...parseFunction(parts), type: 'FunctionExpression' };
+    return { ...parseFunction(parts), type: "FunctionExpression" };
   },
 
   IfStatement(_if, test, consequent, _else, alternate = null) {
-    return { type: 'IfStatement', test, consequent, alternate };
+    return { type: "IfStatement", test, consequent, alternate };
   },
 
   // MapEntry(key, op, value) {
@@ -180,61 +191,69 @@ const processors = {
     const elements = [];
     for (let i = 0; i < entries.length; i += 1) {
       const part = entries[i];
-      if (part !== '}') {
-        if (part === ',' || part === '{') {
-          if (entries[i + 1] === ',') {
+      if (part !== "}") {
+        if (part === "," || part === "{") {
+          if (entries[i + 1] === ",") {
             elements.push(null); // Empty element.
           }
-        } else if (part === '...') {
+        } else if (part === "...") {
           i += 1;
-          elements.push({ type: 'SpreadElement', argument: entries[i] });
-        } else if (entries[i + 1] === '=>'){
+          elements.push({ type: "SpreadElement", argument: entries[i] });
+        } else if (entries[i + 1] === "=>") {
           elements.push({
-            type: 'ArrayExpression',
-            elements:[part, entries[i + 2]]
+            type: "ArrayExpression",
+            elements: [part, entries[i + 2]],
           });
         } else {
           continue;
         }
       }
     }
-    return { type: 'NewExpression', 
-    callee: { type: 'Identifier', name: 'Map' }, 
-    arguments: [
-      {
-        type: 'ArrayExpression',
-        elements
-      }      
-    ]};
-    console.log(elements);
-    return { type: 'MapExpression', entries};
+    if (elements.length > 0) {
+      return {
+        type: "NewExpression",
+        callee: { type: "Identifier", name: "Map" },
+        arguments: [
+          {
+            type: "ArrayExpression",
+            elements,
+          },
+        ],
+      };
+    } else {
+      return {
+        type: "NewExpression",
+        callee: { type: "Identifier", name: "Map" },
+        arguments: [],
+      };
+    }
   },
 
   MemberExpression(object, op, property) {
-    const computed = op === '[';
-    return { type: 'MemberExpression', object, property, computed };
+    const computed = op === "[";
+    return { type: "MemberExpression", object, property, computed };
   },
 
   NullLiteral() {
-    return { type: 'Literal', value: null };
+    return { type: "Literal", value: null };
   },
 
   NewExpression(_new, callee, args) {
-    return { type: 'NewExpression', callee, arguments: args };
+    return { type: "NewExpression", callee, arguments: args };
   },
 
   Number(raw) {
-    const value = /n$/.test(raw) ? BigInt(raw.replace(/n$/, '')) : +raw;
-    return { type: 'Literal', value, raw };
+    const value = /n$/.test(raw) ? BigInt(raw.replace(/n$/, "")) : +raw;
+    return { type: "Literal", value, raw };
   },
 
   ObjectExpression(_ob, ...properties) {
-    properties = properties.filter((prop) => prop !== ',' && prop !== '}');
-    return { type: 'ObjectExpression', properties };
+    properties = properties.filter((prop) => prop !== "," && prop !== "}");
+    return { type: "ObjectExpression", properties };
   },
 
   ParamList(_op, ...parts) {
-    return parts.filter((part) => part !== ',' && part !== ')');
+    return parts.filter((part) => part !== "," && part !== ")");
   },
 
   ParenthesizedExpression(_op, exp, _cp) {
@@ -242,41 +261,49 @@ const processors = {
   },
 
   PostfixExpression(argument, operator) {
-    const type = 'UpdateExpression';
+    const type = "UpdateExpression";
     return { type, prefix: false, operator, argument };
   },
 
   Property(...args) {
     let computed = false;
-    const kind = 'init';
+    const kind = "init";
     const method = false;
     const shorthand = false;
-    if (args[0] === '[') {
+    if (args[0] === "[") {
       const [_ob, key, _cb, _colon, value] = args;
       computed = true;
-      return { type: 'Property', key, value, computed, kind, method, shorthand };
+      return {
+        type: "Property",
+        key,
+        value,
+        computed,
+        kind,
+        method,
+        shorthand,
+      };
     }
     const [key, _colon, value] = args;
-    return { type: 'Property', key, value, computed, kind, method, shorthand };
+    return { type: "Property", key, value, computed, kind, method, shorthand };
   },
 
   PropertyName(name) {
-    return { type: 'Identifier', name };
+    return { type: "Identifier", name };
   },
 
   PropertyNameDefinition(name) {
-    return { type: 'Identifier', name };
+    return { type: "Identifier", name };
   },
 
   RegExp(raw) {
     // eslint-disable-next-line no-eval
     const value = eval(raw);
     const regex = { pattern: value.source, flags: value.flags };
-    return { type: 'Literal', raw, value, regex };
+    return { type: "Literal", raw, value, regex };
   },
 
   ReturnStatement(_return, argument) {
-    return { type: 'ReturnStatement', argument };
+    return { type: "ReturnStatement", argument };
   },
 
   // SetEntry (fst, snd) {
@@ -292,123 +319,136 @@ const processors = {
     const elements = [];
     for (let i = 0; i < parts.length; i += 1) {
       const part = parts[i];
-      if (part !== '}') {
-        if (part === ',' || part === '{') {
-          if (parts[i + 1] === ',') {
+      if (part !== "}") {
+        if (part === "," || part === "{") {
+          if (parts[i + 1] === ",") {
             elements.push(null); // Empty element.
           }
-        } else if (part === '...') {
+        } else if (part === "...") {
           i += 1;
-          elements.push({ type: 'SpreadElement', argument: parts[i] });
+          elements.push({ type: "SpreadElement", argument: parts[i] });
         } else {
           elements.push(part);
         }
       }
     }
-    return { type: 'NewExpression', 
-    callee: { type: 'Identifier', name: 'Set' }, 
-    arguments: [
-      {
-        type: 'ArrayExpression',
-        elements
-      }      
-    ]};
+    if (elements.length > 0) {
+      return {
+        type: "NewExpression",
+        callee: { type: "Identifier", name: "Set" },
+        arguments: [
+          {
+            type: "ArrayExpression",
+            elements,
+          },
+        ],
+      };
+    } else {
+      return {
+        type: "NewExpression",
+        callee: { type: "Identifier", name: "Set" },
+        arguments: [],
+      };
+    }
   },
 
   Script(...body) {
-    body = body.filter((elem) => elem !== ';');
-    return { type: 'Program', sourceType: 'script', body };
+    body = body.filter((elem) => elem !== ";");
+    return { type: "Program", sourceType: "script", body };
   },
 
   SequenceExpression(...exps) {
-    const expressions = exps.filter((part) => part !== ',');
-    return { type: 'SequenceExpression', expressions };
+    const expressions = exps.filter((part) => part !== ",");
+    return { type: "SequenceExpression", expressions };
   },
 
   String(raw) {
     // eslint-disable-next-line no-eval
-    return { type: 'Literal', value: eval(raw), raw };
+    return { type: "Literal", value: eval(raw), raw };
   },
 
   ThrowStatement(_throw, argument) {
-    return { type: 'ThrowStatement', argument };
+    return { type: "ThrowStatement", argument };
   },
 
   TryStatement(_try, block, ...parts) {
     let handler = null;
-    if (parts[0] === 'catch') {
+    if (parts[0] === "catch") {
       parts.shift();
       let param = null;
-      if (parts[0] === '(') {
+      if (parts[0] === "(") {
         [, param] = parts.splice(0, 3);
       }
-      handler = { type: 'CatchClause', param, body: parts.shift() };
+      handler = { type: "CatchClause", param, body: parts.shift() };
     }
     let finalizer = null;
-    if (parts[0] === 'finally') {
+    if (parts[0] === "finally") {
       [, finalizer] = parts.splice(0, 2);
     }
-    return { type: 'TryStatement', block, handler, finalizer };
+    return { type: "TryStatement", block, handler, finalizer };
   },
 
   UnaryExpression(t1, t2) {
-    const prefix = typeof t1 === 'string';
+    const prefix = typeof t1 === "string";
     const operator = prefix ? t1 : t2;
     const argument = prefix ? t2 : t1;
-    if (operator === 'await') {
-      return { type: 'AwaitExpression', argument };
+    if (operator === "await") {
+      return { type: "AwaitExpression", argument };
     }
-    if (operator === 'yield') {
+    if (operator === "yield") {
       const delegate = false;
-      return { type: 'YieldExpression', argument, delegate };
+      return { type: "YieldExpression", argument, delegate };
     }
-    const type = /^(--|\+\+)$/.test(operator) ? 'UpdateExpression'
-      : 'UnaryExpression';
+    const type = /^(--|\+\+)$/.test(operator)
+      ? "UpdateExpression"
+      : "UnaryExpression";
     return { type, prefix, operator, argument };
   },
 
   VariableDeclaration(kind, ...defs) {
     const declarations = [];
     let op;
-    if (kind === "var"){
+    if (kind === "var") {
       kind = "let";
     }
     do {
-      const decl = { type: 'VariableDeclarator', id: defs.shift() };
+      const decl = { type: "VariableDeclarator", id: defs.shift() };
       op = defs.shift();
-      if (op === '=') {
+      if (op === "=") {
         decl.init = defs.shift();
         op = defs.shift();
       } else {
         decl.init = null;
       }
       declarations.push(decl);
-    } while (op === ',');
-    return { type: 'VariableDeclaration', kind, declarations };
+    } while (op === ",");
+    return { type: "VariableDeclaration", kind, declarations };
   },
 
   VariableDefinition(name) {
-    return { type: 'Identifier', name };
+    return { type: "Identifier", name };
   },
 
   VariableName(name) {
-    if (name === 'this') {
-      return { type: 'ThisExpression' };
+    if (name === "this") {
+      return { type: "ThisExpression" };
     }
-    return { type: 'Identifier', name };
+    return { type: "Identifier", name };
   },
 
   WhileStatement(_while, test, body) {
-    return { type: 'WhileStatement', test, body };
+    return { type: "WhileStatement", test, body };
   },
 
   WithStatement(_with, object, body) {
-    return { type: 'WithStatement', object, body };
+    return { type: "WithStatement", object, body };
   },
 }; // processors
 
-'ArithOp Arrow BitOp CompareOp Equals LogicOp Spread Star UpdateOp'
-  .trim().split(/\s+/).forEach((noterm) => {
+"ArithOp Arrow BitOp CompareOp Equals LogicOp Spread Star UpdateOp"
+  .trim()
+  .split(/\s+/)
+  .forEach((noterm) => {
     processors[noterm] = (token) => token;
   });
 
--- test/specs/parser.test.js
+++ test/specs/parser.test.js
@@ -1,11 +1,11 @@
 /* globals describe, it, expect */
-const { inspect } = require('util');
-const { parse: acornParse } = require('acorn');
-const cloneRoot = require('espurify');
-const { parse } = require('../../src/parser/parser');
+const { inspect } = require("util");
+const { parse: acornParse } = require("acorn");
+const cloneRoot = require("espurify");
+const { parse } = require("../../src/parser/parser");
 
 // TODO this super obj?.prop obj?.['prop']
-const EXPS = `
+const EXPSJS = `
   0 1 3.45 0.8e12 1.22e-7 0xC4f3 100n
   "" "x" "a\\t\\"." '' 'x' '<\\x12\\u1234>'
   /[0-9]+/ /\\d+/gi
@@ -25,11 +25,41 @@ const EXPS = `
   function(){} function\tf(x,y){} async\tfunction(){} function*(){} async\tfunction*(){}
   ()=>{} (x)=>x (x,y)=>(x+y) async()=>{}
   class{} class\tC{} class\textends\tB{} class\tC\textends\tB{}
-`.trim().split(/[ \n\r]+/);
+  new\tSet() new\tSet([1,2,3,4]) new\tSet([..."abc"])
+  new\tMap() new\tMap([['a',1],[3.3,true]]) new\tMap([[1,2],...(new\tMap([[3,4]]))])
+`
+  .trim()
+  .split(/[ \n\r]+/);
 
-const STMTS = `
+const EXPS1S = `
+  0 1 3.45 0.8e12 1.22e-7 0xC4f3 100n
+  "" "x" "a\\t\\"." '' 'x' '<\\x12\\u1234>'
+  /[0-9]+/ /\\d+/gi
+  true false null NaN Infinity undefined
+  +1 -2.3 !false !!true ~1 ++pre --pre post++ post--
+  1+2 1-2 1*2 1/2 1%2 1**2 1<<2 1>>2 1>>>2
+  1==2 1!=2 1===2 1!==2 1<2 1<=2 1>2 1>=2
+  1&2 1|2 1&&2 1||2 1^2 1??2
+  1?2:3 1,2
+  typeof(x) void(0)
+  (x)in(0) (x)instanceof(y)  
+  [] [0] [3,] [0,1,2] [...'abc'] [1,2,...''] [...'',...''] [1,2,] [,,]
+  {} {x:1} {null:7,} {'x':1,['y']:2}
+  obj.prop obj.x.y obj['prop'] obj[0][1] obj[0].x obj.x[0]
+  x=1 x+=1 x-=1 x*=1 x/=1 x**=1 x<<=2 x>>=2 x>>>=2 x&=3 x|=3 x^=3 x&&=4 x||=4 x??=4
+  f() f(1) f(1,2) obj.meth() obj.meth(1,2) new\tC() new\tC(1,2)
+  function(){} function\tf(x,y){} async\tfunction(){} function*(){} async\tfunction*(){}
+  ()=>{} (x)=>x (x,y)=>(x+y) async()=>{}
+  class{} class\tC{} class\textends\tB{} class\tC\textends\tB{}
+  Set{} Set{1,2,3,4} Set{..."abc"}
+  Map{} Map{'a'=>1,3.3=>true} Map{1=>2,...Map{3=>4}}
+`
+  .trim()
+  .split(/[ \n\r]+/);
+
+const STMTSJS = `
   return(1); throw(x);
-  var\tx; var\tx=1; let\tx; let\tx=1; const\tx=1;
+  let\tx; let\tx=1; const\tx=1;
   if(1)x=1;
   if(1)x=0;else{x=1;}
   while(0){} while(0){continue;} while(0){break;} do{}while(0);
@@ -38,7 +68,24 @@ const STMTS = `
   try{}catch(e){} try{}catch{} try{}finally{} try{}catch{}finally{}
   with(x){} debugger;
   class\tC{} class\tC\textends\tB{}
-`.trim().split(/[ \n\r]+/);
+`
+  .trim()
+  .split(/[ \n\r]+/);
+
+const STMTS1S = `
+  return(1); throw(x);
+  var\tx; var\tx=1; const\tx=1;
+  if(1)x=1;
+  if(1)x=0;else{x=1;}
+  while(0){} while(0){continue;} while(0){break;} do{}while(0);
+  for(;;){} for(var\tx=0;;){} for(var\tx=0;x<7;){} for(var\tx=0;x<7;x++){}
+  for(x\tin\ty){} for(x\tof\ty){}
+  try{}catch(e){} try{}catch{} try{}finally{} try{}catch{}finally{}
+  with(x){} debugger;
+  class\tC{} class\tC\textends\tB{}
+`
+  .trim()
+  .split(/[ \n\r]+/);
 
 const testCase = (test) => {
   const expected = cloneRoot(acornParse(`${test}`, { ecmaVersion: 2021 }));
@@ -46,30 +93,74 @@ const testCase = (test) => {
   try {
     received = cloneRoot(parse(`${test}`));
   } catch (error) {
-    throw new Error(`Lezer failed!\n${test}\n\n${inspect(expected, false, 10)}\n\n${error}`);
+    throw new Error(
+      `Lezer failed!\n${test}\n\n${inspect(expected, false, 10)}\n\n${error}`
+    );
   }
   expect(received).toEqual(expected);
 };
 
-describe('Lezer parser', () => {
-  it('parses expresions properly', () => {
+const testCaseExps = (jscode, oscode) => {
+  for (let i = 0; i < oscode.length; i++) {
+    const expected = cloneRoot(
+      acornParse(`(${jscode[i]});`, { ecmaVersion: 2021 })
+    );
+    let received;
+    try {
+      received = cloneRoot(parse(`(${oscode[i]});`));
+    } catch (error) {
+      throw new Error(
+        `Lezer failed!\n(${oscode[i]});\n\n${inspect(
+          expected,
+          false,
+          10
+        )}\n\n${error}`
+      );
+    }
+    expect(received).toEqual(expected);
+  }
+};
+
+const testCaseStmts = (jscode, oscode) => {
+  for (let i = 0; i < oscode.length; i++) {
+    const expected = cloneRoot(
+      acornParse(`function _() {${jscode[i]}}`, { ecmaVersion: 2021 })
+    );
+    let received;
+    try {
+      received = cloneRoot(parse(`function _() {${oscode[i]}}`));
+    } catch (error) {
+      throw new Error(
+        `Lezer failed!\nfunction _() {${oscode[i]}}\n\n${inspect(
+          expected,
+          false,
+          10
+        )}\n\n${error}`
+      );
+    }
+    expect(received).toEqual(expected);
+  }
+};
+
+describe("Lezer parser", () => {
+  it("parses expresions properly", () => {
     // Expression tests cases, as expression statements.
-    EXPS.map((exp) => `(${exp});`).forEach(testCase);
+    testCaseExps(EXPSJS, EXPS1S);
   });
 
-  it('parses statements properly', () => {
-    STMTS.map((stmt) => `function _() {${stmt}}`).forEach(testCase);
+  it("parses statements properly", () => {
+    testCaseStmts(STMTSJS, STMTS1S);
   });
 
-  it('parses yield statements properly', () => {
-    [
-      'yield x;', 'yield* x;',
-    ].map((stmt) => `function* _() {${stmt}}`).forEach(testCase);
+  it("parses yield statements properly", () => {
+    ["yield x;", "yield* x;"]
+      .map((stmt) => `function* _() {${stmt}}`)
+      .forEach(testCase);
   });
 
-  it('parses async statements properly', () => {
-    [
-      'await x;', 'for\tawait(x\tof\ty){}',
-    ].map((stmt) => `async function _() {${stmt}}`).forEach(testCase);
+  it("parses async statements properly", () => {
+    ["await x;", "for\tawait(x\tof\ty){}"]
+      .map((stmt) => `async function _() {${stmt}}`)
+      .forEach(testCase);
   });
 });
