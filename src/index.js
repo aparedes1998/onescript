e4a9df4df88fb25d5becc3b7618357f277fb44a6
Agustin Paredes <aparedes1998@hotmail.com>
2021-11-24 17:09:32 -0300

    Added extras

--- src/index.js
+++ src/index.js
@@ -1,73 +1,87 @@
 #!/usr/bin/env node
 
-const { promises: fs } = require('fs');
-const { inspect } = require('util');
-const { parse } = require('./parser/parser');
-const { generate } = require('escodegen');
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers');
-const { argv } = require('process');
+const { promises: fs } = require("fs");
+const { inspect } = require("util");
+const { parse } = require("./parser/parser");
+const { generate, FORMAT_MINIFY } = require("escodegen");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const { argv } = require("process");
 const prompt = require("prompt-sync")();
-
-
 
 function main() {
   let source;
   let output;
 
-
   const argv = yargs(hideBin(process.argv))
-  .option('source', {
-    alias: 's',
-    type: 'string',
-    description: 'Run with source logging'
-  })
-  .option('output', {
-    alias: 'o',
-    type: 'string',
-    description: 'Run with output logging'
-  })
-  .option('log', {
-    type: 'string',
-    description: 'Run with output logging'
-  })
-  .parse();
+    .option("source", {
+      alias: "s",
+      type: "string",
+      description: "Run with source logging",
+    })
+    .option("output", {
+      alias: "o",
+      type: "string",
+      description: "Run with output logging",
+    })
+    .option("log", {
+      type: "string",
+      description: "Run with output logging",
+    })
+    .parse();
 
-  if (argv.source){
+  if (argv.source) {
     source = argv.source;
-  }
-  else{ 
+  } else {
     source = prompt("Escriba su código a continuación: ");
   }
 
-  const result = parse(source);
- 
+  const parsedResult = parse(source);
+  let result;
+  if (argv.minify) {
+    result = generate(parsedResult, { format: FORMAT_MINIFY });
+  } else {
+    result = generate(parsedResult);
+  }
 
-  if (argv.output){
+  if (argv.ast) {
+    fs.writeFile(
+      "output.json",
+      JSON.stringify(parsedResult, null, 4),
+      "utf8",
+      (err) => {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Generated output.json successfully");
+      }
+    );
+  }
+
+  if (argv.output) {
+    if (argv.minify) {
+    }
     output = argv.output;
-  }
-  else {
-    console.log(generate(result));
+  } else {
+    console.log(result);
   }
 
-  if (argv.log){
+  if (argv.log) {
     // Loggear cosas
   }
-  
 
-  if (output){
-    fs.writeFile(output, generate(result), 'utf8', (err) => {
+  if (output) {
+    fs.writeFile(output, result, "utf8", (err) => {
       if (err) {
-          return console.log(err);
+        return console.log(err);
       }
-      console.log("Generated output.js successfully")
+      console.log("Generated output.js successfully");
     });
   }
   //console.log(generate(result));
   //console.log(inspect(result, false, 20));
 }
 //main();
-
 
 if (require.main === module) {
   main();
